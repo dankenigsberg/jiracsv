b8dbd015eac8883b9763e2f6a9e93e4f88514dbf
Rick Garcia <rgarcia@localhost.localdomain>
2020-04-29 12:00:56 -0400

    modified:   client.go 	modified:   issue.go

--- jira/client.go
+++ jira/client.go
@@ -62,6 +62,8 @@ func NewClient(url string, username, password *string) (*Client, error) {
 			client.CustomFieldID.StoryPoints = f.ID
 		case "5-Acks Check":
 			client.CustomFieldID.AckFlags = f.ID
+		case "Ready-Ready":
+			client.CustomFieldID.AckFlags = f.ID
 		case "QA Contact":
 			client.CustomFieldID.QAContact = f.ID
 		case "Acceptance Criteria":
@@ -120,11 +122,13 @@ func (c *Client) FindIssues(jql string) (IssueCollection, error) {
 				storyPoints = int(val.(float64))
 			}
 
-			issueApprovals := IssueApprovals{false, false, false, false, false}
+			issueApprovals := IssueApprovals{false, false, false, false, false, false}
 
 			if val := i.Fields.Unknowns[c.CustomFieldID.AckFlags]; val != nil {
 				for _, p := range val.([]interface{}) {
 					switch p.(map[string]interface{})["value"].(string) {
+					//The Old Ones
+					//-->
 					case "devel_ack":
 						issueApprovals.Development = true
 					case "pm_ack":
@@ -135,6 +139,32 @@ func (c *Client) FindIssues(jql string) (IssueCollection, error) {
 						issueApprovals.Experience = true
 					case "doc_ack":
 						issueApprovals.Documentation = true
+					//<--
+					//The New Ones
+					//-->
+					case "dev-ack":
+						issueApprovals.Development = true
+					case "pm-ack":
+						issueApprovals.Product = true
+					case "qa-ack":
+						issueApprovals.Quality = true
+					case "ux-ack":
+						issueApprovals.Experience = true
+					case "doc-ack":
+						issueApprovals.Documentation = true
+					case "dev-ready":
+						issueApprovals.Development = true
+					case "pm-ready":
+						issueApprovals.Product = true
+					case "qa-ready":
+						issueApprovals.Quality = true
+					case "ux-ready":
+						issueApprovals.Experience = true
+					case "doc-ready":
+						issueApprovals.Documentation = true
+					case "px-ready":
+						issueApprovals.Support = true
+						//<-
 					}
 				}
 			}
